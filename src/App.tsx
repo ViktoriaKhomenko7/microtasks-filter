86b84826d68d8ba40b2b8ff4b3797973945e5462
Viktoria Khomenko <Vikky.kh.28@yandex.ru>
2023-10-14 15:10:29 +0300

    create filtered array with buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,58 @@
 import React, {useState} from 'react';
 import './App.css';
 
+
+type FilterType = "all" | "dollar" | "ruble"
+
 function App() {
-  const [money, setMoney] = useState([
-    {banknote: "dollar", nominal: 100, number: "a123456789"},
-    {banknote: "dollar", nominal: 50, number: "b123456789"},
-    {banknote: "ruble", nominal: 100, number: "c123456789"},
-    {banknote: "dollar", nominal: 100, number: "d123456789"},
-    {banknote: "dollar", nominal: 50, number: "e123456789"},
-    {banknote: "ruble", nominal: 100, number: "f123456789"},
-    {banknote: "dollar", nominal: 50, number: "j123456789"},
-    {banknote: "ruble", nominal: 50, number: "h123456789"}
-  ])
+    const [money, setMoney] = useState([
+        {banknote: "dollar", nominal: 100, number: "a123456789"},
+        {banknote: "dollar", nominal: 50, number: "b123456789"},
+        {banknote: "ruble", nominal: 100, number: "c123456789"},
+        {banknote: "dollar", nominal: 100, number: "d123456789"},
+        {banknote: "dollar", nominal: 50, number: "e123456789"},
+        {banknote: "ruble", nominal: 100, number: "f123456789"},
+        {banknote: "dollar", nominal: 50, number: "j123456789"},
+        {banknote: "ruble", nominal: 50, number: "h123456789"}
+    ])
 
-  let currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === "ruble")
+    const[filter, setFilter] = useState<FilterType>("all")
 
-  return (
-      <ul>
-        {currentMoney.map((objFromMoneyArr, index) => {
-          return (
-              <li key={index}>
-                <span>{objFromMoneyArr.banknote}</span>
-                <span>{objFromMoneyArr.nominal}</span>
-                <span>{objFromMoneyArr.number}</span>
-              </li>
-          )
-        })}
-      </ul>
-  );
+    let currentMoney = money;
+// если nameButton = "dollar"
+    if(filter==="dollar"){
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === "dollar");
+    }
+// если nameButton = "ruble"
+    if(filter==="ruble"){
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknote === "ruble");
+    }
+
+    const onclickFilterHandler = (nameButton: FilterType) => {
+        setFilter(nameButton)
+    }
+
+
+    return (
+        <>
+            <ul>
+                {currentMoney.map((objFromMoneyArr, index) => {
+                    return (
+                        <li key={index}>
+                            <span>{objFromMoneyArr.banknote}</span>
+                            <span>{objFromMoneyArr.nominal}</span>
+                            <span>{objFromMoneyArr.number}</span>
+                        </li>
+                    )
+                })}
+            </ul>
+            <div style={{marginLeft: "45px"}}>
+                <button onClick={()=>onclickFilterHandler("all")}>all</button>
+                <button onClick={()=>onclickFilterHandler("ruble")}>ruble</button>
+                <button onClick={()=>onclickFilterHandler("dollar")}>dollar</button>
+            </div>
+        </>
+    );
 }
 
 export default App;
